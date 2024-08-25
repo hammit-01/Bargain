813ad8259c5600689b96f1cdc901920be2988b0c
hammit <mpa0530@gmail.com>
2024-08-25 15:17:02 +0900

    first commit to connect with GitHub!
     Conflicts:
    	.idea/gradle.xml
    	.idea/misc.xml
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/auction/DetailMenuBottom.kt
    	app/src/main/java/com/example/auction/MainActivity.kt
    	app/src/main/java/com/example/auction/adapter/DetailAdapter.kt
    	app/src/main/java/com/example/auction/experience/ActivityDetail.kt
    	app/src/main/java/com/example/auction/experience/ActivityPage.kt
    	app/src/main/java/com/example/auction/experience/AppliActivity.kt
    	app/src/main/java/com/example/auction/productpage/BuyActivity.kt
    	app/src/main/java/com/example/auction/productpage/DetailActivity.kt
    	app/src/main/java/com/example/auction/productpage/ProductActivity.kt
    	app/src/main/java/com/example/auction/usermanage/LoginActivity.kt
    	app/src/main/res/layout/activity_detail.xml
    	app/src/main/res/layout/activity_list.xml
    	app/src/main/res/layout/activity_main.xml
    	app/src/main/res/layout/appli_detail.xml
    	app/src/main/res/layout/buy_detail.xml
    	app/src/main/res/layout/login_page.xml
    	app/src/main/res/layout/page_detail.xml
    	app/src/main/res/layout/product_page.xml
    	app/src/main/res/menu/bottom_nav_menu.xml
    	app/src/main/res/values-night/themes.xml
    	app/src/main/res/values/colors.xml
    	app/src/main/res/values/themes.xml
    	build.gradle.kts
    	gradle.properties
    	gradle/wrapper/gradle-wrapper.properties
    	settings.gradle.kts

--- app/src/main/java/com/example/auction/MainActivity.kt
+++ app/src/main/java/com/example/auction/MainActivity.kt
@@ -14,8 +14,6 @@ import com.example.auction.productpage.ProductActivity
 import com.example.auction.usermanage.LoginActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// hi
-
 // 아래는 주석문 매뉴얼입니다
 //File Name: MainActivity.kt
 //Author: 김혜미
