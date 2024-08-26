a69fc5539dbf1263d4db4618dd2978c0ade041cc
hammit <mpa0530@gmail.com>
2024-08-26 21:17:21 +0900

    튜토리얼 화면 버튼 수정, Todo 추가

--- app/src/main/java/com/example/auction/tutorial/TutorialActivity.kt
+++ app/src/main/java/com/example/auction/tutorial/TutorialActivity.kt
@@ -26,6 +26,8 @@ class TutorialActivity : AppCompatActivity() {
         tabLayout = findViewById(R.id.tab_layout)
         btnGetStarted = findViewById(R.id.btn_get_started)
 
+        // Todo app 최초 접속 시에만 튜토리얼 화면 띄우게끔 사용자 로그 관리
+        // (app을 두번째로 실행하면 회원가입을 안하거나 로그인을 안해도 튜토리얼 화면이 안뜸)
         val tutorialImages = listOf(
             R.drawable.tutorial_image1,
             R.drawable.tutorial_image2,
